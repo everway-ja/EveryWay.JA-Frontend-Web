8543246f7a6b6d14ddee2b5e1282a9c063584497
Teooo06 <matteobertoldini06@gmail.com>
2025-03-26 12:06:24 +0100

    Section Mappa Finita??

--- src/components/Mappa.jsx
+++ src/components/Mappa.jsx
@@ -8,12 +8,17 @@ const Mappa = ({title}) => {
             <div className="blocco-ordina">
                 <div className="blocco-a">
                     <div className="image-1">
-
+                        <img src={"https://plus.unsplash.com/premium_photo-1661962830760-1e0a679eba23?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+                             alt={"test"} className="img"></img>
                     </div>
                     <div className="image-4">
+                        <img src={"https://images.unsplash.com/photo-1608022989714-e1fd968552c5?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+                             alt={"test"} className="img"></img>
 
                     </div>
                     <div className="image-2">
+                        <img src={"https://plus.unsplash.com/premium_photo-1690552155363-7d6a2ed5d645?q=80&w=2972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+                             alt={"test"} className="img"></img>
 
                     </div>
                     <div className="box-title">
@@ -22,9 +27,8 @@ const Mappa = ({title}) => {
                 </div>
                 <div className="blocco-b1">
                     <div className="image-3">
-
-                    </div>
-                    <div className="">
+                        <img src={"https://plus.unsplash.com/premium_photo-1690474614774-a7d16ee9f2dd?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+                             alt={"test"} className="img"></img>
 
                     </div>
                 </div>
@@ -32,9 +36,13 @@ const Mappa = ({title}) => {
             <div className="blocco-ordina">
                 <div className="blocco-c">
                     <div className="image-5">
+                        <img src={"https://images.unsplash.com/photo-1706631519116-6607d09107b2?q=80&w=3131&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+                             alt={"test"} className="img"></img>
 
                     </div>
                     <div className="image-6">
+                        <img src={"https://images.unsplash.com/photo-1563309480-5aca14189417?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+                             alt={"test"} className="img"></img>
 
                     </div>
                 </div>
