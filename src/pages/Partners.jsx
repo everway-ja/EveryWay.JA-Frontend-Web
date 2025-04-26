37512ff6ca79e3c896b6e4741b0916b9e2a180a7
LorenBll <lorenzo.bellotta@hotmail.com>
2025-04-26 21:15:01 +0200

    Reseet Start.

--- src/pages/Partners.jsx
+++
@@ -1,8 +0,0 @@
-function Partners() {
-  return (
-      <div className="flex flex-col items-center justify-center h-screen">
-          <h1>Partners</h1>
-      </div>
-  );
-}
-export default Partners;
